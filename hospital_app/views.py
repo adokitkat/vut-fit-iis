5d533d54e57c769b45a6ddcf8f80d58d3412b02e
Adam Múdry <mudry.ado@gmail.com>
2020-11-23 04:19:43 +0100

    authorization update

--- hospital_app/views.py
+++ hospital_app/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.contrib.auth.decorators import login_required, permission_required
 
+from .forms import CustomUserCreationForm
 from .models import CustomUser
+
 # Temporary view
 from django.http import HttpResponse
 #@login_required
@@ -12,3 +14,17 @@ def index(request):
     users = CustomUser.objects.all()
     context = {'Users': users}
     return render(request, 'hospital_app/index.html', context)
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            #username = form.cleaned_data.get('username')
+            #raw_password = form.cleaned_data.get('password1')
+            #user = authenticate(username=username, password=raw_password)
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            return redirect('index')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'hospital_app/signup.html', {'form': form})
